1c614c3d1efe9a583a961c97f14df1d99464f2e3
zstu21 <3435657471@qq.com>
2024-01-10 03:42:51 +0800

    Site updated: 2024-01-10 03:42:50

--- js/newYear.js
+++ js/newYear.js
@@ -1,4 +1,4 @@
-let newYearTimer = null;
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>let newYearTimer = null;
 var newYear = () => {
     clearTimeout(newYearTimer);
     if (!document.querySelector('#newYear')) return;
